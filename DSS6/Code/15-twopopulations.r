117400dd12cbd18df8b7e89299a1550db4541956
Dublin R <DublinRUserGroup@gmail.com>
2014-10-20 13:22:04 +0100

    Update 14-twopopulations.r

--- DSS6/Code/15-twopopulations.r
+++ DSS6/Code/15-twopopulations.r
@@ -7,12 +7,11 @@ rbinom(60000,1,p) ->  Pop.F
 addmargins(table(Pop.M))
 addmargins(table(Pop.F))
 
-
 Samp1.M = sample(Pop.M,200)
 Samp1.F = sample(Pop.F,200)
 
-table(Samp1.M)
-table(Samp1.F)
+addmargins(table(Samp1.M))
+addmargins(table(Samp1.F))
 
 
 Samp2.M = sample(Pop.M,200)
@@ -20,3 +19,7 @@ Samp2.F = sample(Pop.F,200)
 
 addmargins(table(Samp2.M))
 addmargins(table(Samp2.F))
+
+prop.test(c(23,28),c(200,200))
+
+prop.test(c(13,32),c(200,200))
