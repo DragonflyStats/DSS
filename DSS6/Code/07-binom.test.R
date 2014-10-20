fabdfd8713261702b2c82f60764e8b285df78fef
Dublin R <DublinRUserGroup@gmail.com>
2014-10-20 10:12:33 +0100

    Update 07-binom.test.R

--- DSS6/Code/07-binom.test.R
+++ DSS6/Code/07-binom.test.R
@@ -11,3 +11,17 @@ binom.test(x = 3, n = 4, p=.5, alternative="greater")
 # is either greater than p or that it is less than p.
 
 help(binom.test)
+
+
+## Conover (1971), p. 97f.
+## Under (the assumption of) simple Mendelian inheritance, a cross
+##  between plants of two particular genotypes produces progeny 1/4 of
+##  which are "dwarf" and 3/4 of which are "giant", respectively.
+##  In an experiment to determine if this assumption is reasonable, a
+##  cross results in progeny having 243 dwarf and 682 giant plants.
+##  If "giant" is taken as success, the null hypothesis is that p =
+##  3/4 and the alternative that p != 3/4.
+binom.test(c(682, 243), p = 3/4)
+binom.test(682, 682 + 243, p = 3/4)   # The same.
+## => Data are in agreement with the null hypothesis.
+
