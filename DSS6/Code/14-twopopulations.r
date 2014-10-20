57ab47b324414e94bdded13c27652a5d4861b3e6
Dublin R <DublinRUserGroup@gmail.com>
2014-10-20 10:34:20 +0100

    Update 14-twopopulations.r

--- DSS6/Code/14-twopopulations.r
+++ DSS6/Code/14-twopopulations.r
@@ -4,8 +4,8 @@ p=runif(1)
 rbinom(60000,1,p) ->  Pop.M
 rbinom(60000,1,p) ->  Pop.F
 
-table(Pop.M)
-table(Pop.F)
+addmargins(table(Pop.M))
+addmargins(table(Pop.F))
 
 
 Samp1.M = sample(Pop.M,200)
@@ -18,5 +18,5 @@ table(Samp1.F)
 Samp2.M = sample(Pop.M,200)
 Samp2.F = sample(Pop.F,200)
 
-table(Samp2.M)
-table(Samp2.F)
+addmargins(table(Samp2.M))
+addmargins(table(Samp2.F))
