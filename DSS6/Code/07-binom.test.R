fd0ae236bc82a32569aaab3f3c47058db75ad503
Dublin R <DublinRUserGroup@gmail.com>
2014-10-20 10:11:56 +0100

    Create new file

---
+++ DSS6/Code/07-binom.test.R
@@ -0,0 +1,13 @@
+binom.test(x = 3, n = 4, p=.5, alternative="greater")
+
+# Binomial test
+ 
+ # http://www.instantr.com/2012/11/06/performing-a-binomial-test/
+ 
+# A binomial test compares the number of successes observed in a given number of trials with a hypothesised probability of success.
+# The test has the null hypothesis that the real probability of success is equal to some value denoted p, and the alternative
+# hypothesis that it is not equal to p. 
+# The test can also be performed with a one-sided alternative hypothesis that the real probability of success 
+# is either greater than p or that it is less than p.
+
+help(binom.test)
