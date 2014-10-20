d3ddd616435e1a7cb34357fc98a2be47b7e2e400
Dublin R <DublinRUserGroup@gmail.com>
2014-10-20 09:49:09 +0100

    Create new file

---
+++ DSS6/Code/05-nortest.R
@@ -0,0 +1,11 @@
+# nortest: Tests for Normality
+ 
+# Five omnibus tests for the composite hypothesis of normality
+
+library(nortest)
+
+help( ad.test ) # Anderson Darling Test
+help( cvm.test )
+help( lillie.test ) 
+help( pearson.test )
+help( sf.test )
